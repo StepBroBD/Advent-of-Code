122fd09d02870576e13156f3ecb1b4be9fbbfc10
StepBroBD <Hi@StepBroBD.com>
2021-12-01 23:40:07 -0700

    changed day 2 solution format

--- aoc2021/src/day_02/solution.rs
+++ aoc2021/src/day_02/solution.rs
@@ -6,7 +6,7 @@ pub fn part_01(input: &Vec<String>) -> i64 {
     let mut x: i64 = 0;
     let mut y: i64 = 0;
     for i in 0..input.len() {
-        let mut instructions: Vec<&str> = input[i].split_whitespace().collect();
+        let instructions: Vec<&str> = input[i].split_whitespace().collect();
         if instructions[0] == "forward" {
             x += i64::from_str(instructions[1]).unwrap();
         }
@@ -26,7 +26,7 @@ pub fn part_02(input: &Vec<String>) -> i64 {
     let mut y: i64 = 0;
     let mut aim: i64 = 0;
     for i in 0..input.len() {
-        let mut instructions: Vec<&str> = input[i].split_whitespace().collect();
+        let instructions: Vec<&str> = input[i].split_whitespace().collect();
         if instructions[0] == "forward" {
             x += i64::from_str(instructions[1]).unwrap();
             y += aim * i64::from_str(instructions[1]).unwrap();
@@ -43,8 +43,6 @@ pub fn part_02(input: &Vec<String>) -> i64 {
 
 #[cfg(test)]
 mod tests {
-    use colored::*;
-
     use util::io::input_parser::InputParser;
 
     use crate::day_02::solution::{part_01, part_02};
@@ -52,7 +50,6 @@ mod tests {
     #[test]
     fn test_aoc_2021_day_02_part_01() {
         let data = InputParser::<String>::data("../aoc2021/src/day_02/input");
-        println!("{}", "##################################################".green());
         println!("AoC 2021 Day 2 Part 1: {}.", part_01(&data));
     }
 
@@ -60,6 +57,5 @@ mod tests {
     fn test_aoc_2021_day_02_part_02() {
         let data = InputParser::<String>::data("../aoc2021/src/day_02/input");
         println!("AoC 2021 Day 2 Part 2: {}.", part_02(&data));
-        println!("{}", "##################################################".green());
     }
 }
